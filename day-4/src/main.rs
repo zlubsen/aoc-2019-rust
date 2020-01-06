370580fc06f1685e55d59198118a30ade125de49
Zeeger <zeeger@lubsen.eu>
2020-01-06 14:06:38 +0100

    Day 4 - part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,24 +1,36 @@
 use std::str::FromStr;
+use regex::Regex;
 
 fn main() {
     let range_from = 145852;
     let range_to = 616942;
 
-    let mut valid_pwds = 0;
+    let mut valid_pwds_pt_1 = 0;
+    let mut valid_pwds_pt_2 = 0;
 
     for i in range_from..=range_to {
+        if i % 20000 == 0 { print!("."); }
+
         if is_within_range(i, range_from, range_to)
             && is_six_digit(i)
-            && has_two_adjacent(i)
             && is_increasing(i) {
-            valid_pwds += 1;
+            if has_two_adjacent(i) {
+                valid_pwds_pt_1 += 1;
+                println!("\nFound adjacent!");
+                if has_double(i) {
+                    valid_pwds_pt_2 += 1;
+                    print!("It's a double!");
+                }
+            }
         }
     }
-    println!("There are {} combinations valid", valid_pwds);
+    println!("There are {} combinations valid", valid_pwds_pt_1);
+    println!("There are {} combinations for part 2", valid_pwds_pt_2);
 }
 
 fn is_six_digit(number : i32) -> bool {
-    number >= 100000 && number <= 999999
+    let re = Regex::new(r"^\d{6}$").unwrap();
+    re.is_match(number.to_string().as_str())
 }
 
 fn is_within_range(number : i32, range_from : i32, range_to : i32) -> bool {
@@ -26,6 +38,8 @@ fn is_within_range(number : i32, range_from : i32, range_to : i32) -> bool {
 }
 
 fn has_two_adjacent(number : i32) -> bool {
+//    let re = Regex::new(r"^\d*((\d)\2)\d*$").unwrap();
+//    re.is_match(number.to_string().as_str())
     let text = number.to_string();
     let mut prev_c : Option<char> = Option::None;
     for c in text.chars() {
@@ -41,6 +55,52 @@ fn has_two_adjacent(number : i32) -> bool {
     }
 
     false
+}
+
+fn has_double(number : i32) -> bool {
+//    let re = Regex::new(r"([\d])\1{1}(?!\1)").unwrap();
+//    println!("text: {}", text);
+//    let matches = re.is_match(number.to_string().as_str());
+//    println!("{}", matches);
+
+    // fill a vec with counts of all adjacent characters
+    let text = number.to_string();
+    let mut curr_c: Option<char> = Option::None;
+    let mut count : i32 = 0;
+    let mut adjacents : Vec<(char, i32)> = Vec::new();
+    for c in text.chars() {
+        if curr_c.is_none() {
+            curr_c = Option::Some(c);
+            count += 1;
+        } else {
+            if curr_c.unwrap() == c {
+                count += 1;
+            } else {
+                if count > 1 {
+                    adjacents.push((curr_c.unwrap(), count));
+                }
+                curr_c = Option::Some(c);
+                count = 1;
+            }
+        }
+    }
+    if count > 1 {
+        adjacents.push((curr_c.unwrap(), count));
+    }
+
+    // filter any adjacents > 2
+    let mut doubles : Vec<(char, i32)> = Vec::new();
+    if adjacents.is_empty() {
+        // when no adjacent items found
+        return false
+    } else {
+        for tup in &adjacents {
+            if tup.1 == 2 {
+                doubles.push(*tup);
+            }
+        }
+    }
+    !doubles.is_empty()
 }
 
 fn is_increasing(number : i32) -> bool {
@@ -59,7 +119,7 @@ fn is_increasing(number : i32) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use crate::{has_two_adjacent, is_six_digit, is_within_range, is_increasing};
+    use crate::{has_two_adjacent, is_six_digit, is_within_range, is_increasing, has_double};
 
     #[test]
     fn test_has_two_adjacent() {
@@ -88,5 +148,12 @@ mod tests {
         assert_eq!(is_increasing(111123), true);
         assert_eq!(is_increasing(135679), true);
         assert_eq!(is_increasing(223450), false);
+    }
+
+    #[test]
+    fn test_has_double() {
+        assert_eq!(has_double(112233), true);
+        assert_eq!(has_double(123444), false);
+        assert_eq!(has_double(111122), true);
     }
 }
